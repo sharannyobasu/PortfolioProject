9296d920f03fb104374bfdf3e59bcfc30dccec18
Sharannyo Basu <yobasu2015@gmail.com>
2020-12-23 20:56:08 +0530

    Added dropdown functionality

---
+++ script.js
@@ -0,0 +1,11 @@
+var scrollToTopBtn = document.getElementById("firstdown");
+var rootElement = document.documentElement;
+
+function scrollToTop() {
+  // Scroll to top logic
+  rootElement.scrollTo({
+    top: 1000,
+    behavior: "smooth",
+  });
+}
+scrollToTopBtn.addEventListener("click", scrollToTop);
